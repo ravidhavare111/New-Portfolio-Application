7eade451a7c0b0d99e2f93f2b4d8b0892d6bda31
Ravi Dhavare <100593775+ravidhavare111@users.noreply.github.com>
2025-07-19 17:36:51 +0530

    My Portfolio WebApp

---
+++ src/main/java/in/ravidhavare/portfolio2/Portfolio2Application.java
@@ -0,0 +1,13 @@
+package in.ravidhavare.portfolio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portfolio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portfolio2Application.class, args);
+	}
+
+}
